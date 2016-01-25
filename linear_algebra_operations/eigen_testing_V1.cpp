9988ce1a43db0c5eb203e014eda30b37fb94a999
Jim Thorson <JamesT.esq@gmail.com>
2016-01-25 15:57:20 -0800

    added CPP for testing eigendecomposition

--- linear_algebra_operations/eigen_testing_V1.cpp
+++ linear_algebra_operations/eigen_testing_V1.cpp
@@ -1,5 +1,38 @@
 
 #include <TMB.hpp>
+/** \brief Matrix exponential: matrix of arbitrary dimension. */
+template <class scalartype, int dim>
+struct matexp_mod{
+  typedef Matrix<scalartype,dim,dim> matrix;
+  typedef Matrix<std::complex<scalartype> ,dim,dim> cmatrix;
+  typedef Matrix<std::complex<scalartype> ,dim,1> cvector;
+  cmatrix V;
+  cmatrix iV;
+  cvector lambda;
+  EigenSolver< matrix > eigensolver;
+  matexp(){};
+  matexp(matrix A_){
+    eigensolver.compute(A_);
+    V=eigensolver.eigenvectors();
+    lambda=eigensolver.eigenvalues();
+    iV=V.inverse();
+  }
+  matrix operator()(scalartype t){
+    cmatrix tmp;
+    tmp.setZero();
+    matrix ans;
+    for(int i=0;i<dim;i++)tmp(i,i)=exp(lambda(i)*t);
+    //tmp=V*tmp*iV;
+    tmp=tmp.operator*(iV);
+    tmp=V.operator*(tmp);
+
+    for(int i=0;i<dim;i++)
+      for(int j=0;j<dim;j++)
+	ans(i,j)=std::real(tmp(i,j));
+    return ans;
+  }
+};
+
 template<class Type>
 Type objective_function<Type>::operator() ()
 {
@@ -12,23 +45,15 @@ Type objective_function<Type>::operator() ()
   PARAMETER( sigma2 );
 
   // Probability of random effects
-  matrix<Type> Q_yy( c_i.size(), c_i.size() );
+  int n_y = c_i.size();
+  matrix<Type> Q_yy( n_y, n_y );
   Q_yy.setZero();
-  for(int y=0; y<c_i.size(); y++) Q_yy(y,y) = (1+pow(rho,2))/sigma2;
-  for(int y=1; y<c_i.size(); y++){
+  for(int y=0; y<n_y; y++) Q_yy(y,y) = (1+pow(rho,2))/sigma2;
+  for(int y=1; y<n_y; y++){
     Q_yy(y-1,y) = -1 * rho/sigma2;
     Q_yy(y,y-1) = 1 * rho/sigma2;
   }
   REPORT( Q_yy )
-
-  // EXAMPLE FROM: http://kaskr.github.io/adcomp/matexp_8cpp_source.html
-  //using namespace Eigen;
-  //Eigen::EigenSolver< matrix > eigensolver;
-  //eigensolver.compute( Q_yy );
-  //V = eigensolver.eigenvectors();
-  //lambda=eigensolver.eigenvalues();
-  //vector<Type> eigenvalues_y(n_y);
-  //eigenvalues_y = ;compute().eigenvalues();
 
   // Elementary
   Eigen::SparseMatrix<Type> Qsparse_yy = asSparseMatrix( Q_yy );
@@ -46,17 +71,37 @@ Type objective_function<Type>::operator() ()
   REPORT( V );
 
   // Eigenvalues
-  Eigen::EigenSolver< Eigen::SparseMatrix<Type> > eigensolver;
-  //Eigen::EigenSolver< matrix<Type> > eigensolver;
+  // EXAMPLE AT: https://github.com/kaskr/adcomp/blob/master/TMB/inst/include/tmbutils/matexp.hpp
+  //Eigen::EigenSolver< Eigen::SparseMatrix<Type> > eigensolver;
   //eigensolver.compute( Qsparse_yy, true );
+  Eigen::EigenSolver< matrix<Type> > eigensolver;
+  Eigen::Matrix< Type, Eigen::Dynamic, Eigen::Dynamic > Tmp_yy = Q_yy;
+  //Eigen::EigenSolver< matrix<Type> > eigensolver( Tmp_yy );
+  //eigensolver.compute( Q_yy );
   //matrix<Type> V = eigensolver.eigenvectors();
   //vector<Type> lambda = eigensolver.eigenvalues();
+  //Q_yy.doComputeEigenvectors();
   vector< std::complex< Type > > eigenvalues_Q_yy = Q_yy.eigenvalues();
   vector< Type > real_eigenvalues_Q_yy = eigenvalues_Q_yy.real();
   vector< Type > imag_eigenvalues_Q_yy = eigenvalues_Q_yy.imag();
   REPORT( real_eigenvalues_Q_yy );
   REPORT( imag_eigenvalues_Q_yy );
-  //std::complex<scalartype>
+
+  // Eigenvectors
+  // http://eigen.tuxfamily.org/dox-devel/classEigen_1_1EigenSolver.html#a8c287af80cfd71517094b75dcad2a31b
+  using namespace Eigen;
+  // WORKS
+  MatrixXf A = MatrixXf::Random(4,4);
+  EigenSolver<MatrixXf> es(A);
+  //es.compute(A);
+  es.eigenvectors();
+  // TESTING
+  EigenSolver< matrix<Type> > es_type( );
+  //MatrixXf A = MatrixXf::Random(4,4);
+  //es.compute(Q_yy);
+  //es.eigenvectors();
+
+  // QR decomposition
 
   // Schur decomposition
   // https://github.com/kaskr/adcomp/blob/master/TMB/inst/include/Eigen/src/Eigenvalues/RealSchur.h
@@ -77,7 +122,7 @@ Type objective_function<Type>::operator() ()
   // Probability of data conditional on random effects
   Type Total_Abundance = 0;
   Type jnll = 0;
-  for( int i=0; i<c_i.size(); i++){
+  for( int i=0; i<n_y; i++){
     jnll -= dnorm( c_i(i), beta, Type(1.0), true );
   }
 
