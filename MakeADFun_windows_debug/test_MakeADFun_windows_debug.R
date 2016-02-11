d49e6e3f09dadd56747c8afb1eb85d5c4b1ecd86
Jim Thorson <JamesT.esq@gmail.com>
2016-02-11 15:29:53 -0800

    final (for now) version of the new MakeADFun...
    .... for use by windows

--- MakeADFun_windows_debug/test_MakeADFun_windows_debug.R
+++ MakeADFun_windows_debug/test_MakeADFun_windows_debug.R
@@ -1,5 +1,6 @@
 
-setwd( "C:/Users/James.Thorson/Desktop/Project_git/TMB_experiments/windows_debugger" )
+File = "C:/Users/James.Thorson/Desktop/Project_git/TMB_experiments/windows_debugger"
+setwd( File )
 source( "MakeADFun_windows_debug.R" )
 
 #####################
@@ -19,11 +20,12 @@ Y = Z[Factor] + X0 + rnorm( length(Factor), mean=0, sd=1)
 
 library(TMB)
 cpp_name = "problem"
-compile( paste0(cpp_name,".cpp"), "-O1 -g", DLLFLAGS="")
+#compile( paste0(cpp_name,".cpp"), "-O1 -g", DLLFLAGS="")
+compile( paste0(cpp_name,".cpp") )
 
 data = list( "n_data"=length(Y), "n_factors"=length(unique(Factor)), "Factor"=Factor-1, "Y"=Y)
 parameters = list( "X0"=-10, "log_SD0"=2, "log_SDZ"=2, "Z"=rep(0,data$n_factor) )
 random = c("Z")
 
-dyn.load( dynlib(cpp_name) )
-Output = MakeADFun_windows_debug( data=data, cpp_name=cpp_name, parameters=parameters, random=random )
+#dyn.load( dynlib(cpp_name) )
+Output = MakeADFun_windows_debug( data=data, cpp_name=cpp_name, dir=File, parameters=parameters, random=random, overwrite=TRUE )
